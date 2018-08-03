99919a869b14e2b52937b43cfa72cc14f68722ed
pmotaparthi <pmotaparthi@10.36.101.236>
2018-08-03 13:21:43 +0530

    first commit for test learning project

---
+++ src/main/java/com/example/test/TestController.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+	
+	@RequestMapping("/sample")
+	public String test() {
+		return "This is just a sample text returned from controller";
+	}
+}
