c8cb91abd00d5deb55dd8c624e18b9c7bd29742b
pmotaparthi <pmotaparthi@10.36.101.236>
2018-08-06 17:25:26 +0530

    test

--- src/main/java/com/example/test/TestController.java
+++ src/main/java/com/example/test/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 	public Log log = LogFactory.getLog(TestController.class);
-	
+	//test comment
 	@RequestMapping("/sample")
 	public String test() {
 		log.info("Inside the test controller!!!!!");
